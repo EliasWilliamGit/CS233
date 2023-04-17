d1d588acbb458c819fd93e1fab9dc2d48918b37d
Pruneollier <91121616+Pruneollier@users.noreply.github.com>
2023-04-17 11:39:42 +0200

    Update logistic_regression.py

--- sciper1_sciper2_sciper3_project/src/methods/logistic_regression.py
+++ sciper1_sciper2_sciper3_project/src/methods/logistic_regression.py
@@ -20,6 +20,62 @@ class LogisticRegression(object):
         self.lr = lr
         self.max_iters = max_iters
 
+        
+    def f_softmax(data):
+        """""
+        Softmax function for multi-class logistic regression.
+    
+        Args:
+            data (array): Input data of shape (N, D)
+
+        Returns:
+            array of shape (N, C): Probability array where each value is in the
+                range [0, 1] and each row sums to 1.
+                The row i corresponds to the prediction of the ith data sample, and 
+                the column j to the jth class. So element [i, j] is P(y_i=k | x_i, W)
+        """
+      
+        num = np.exp(data @ W)
+        denum = np.sum(num, axis=1)
+        N = data.shape[0]
+        denum = denum.reshape((N,1))
+    
+        return num/denum
+        
+    def loss_logistic_multi(self, data, labels):
+        """ 
+        Loss function for multi class logistic regression, i.e., multi-class entropy.
+    
+        Args:
+            data (array): Input data of shape (N, D)
+            labels (array): Labels of shape  (N, C)  (in one-hot representation)
+            
+        Returns:
+            float: Loss value 
+        """
+        
+        ln_y = np.log(self.f_softmax(data, self.w))
+
+        return - np.sum(labels * ln_y)
+
+
+    def gradient_logistic_multi(self, data, labels):
+        """
+        Compute the gradient of the entropy for multi-class logistic regression.
+    
+        Args:
+            data (array): Input data of shape (N, D)
+            labels (array): Labels of shape  (N, C)  (in one-hot representation)
+            
+        Returns:
+            grad (np.array): Gradients of shape (D, C)
+        """
+    
+        y = self.f_softmax(data, self.weights)
+
+        return data.T @ (y - labels)
+
+
     def fit(self, training_data, training_labels):
         """
         Trains the model, returns predicted labels for training data.
@@ -30,11 +86,26 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): target of shape (N,)
         """
+
         ##
         ###
         #### WRITE YOUR CODE HERE! 
         ###
         ##
+
+        D = training_data.shape[1]  # number of features
+        C = training_labels.shape[1]  # number of classes
+
+        # Random initialization of the weights
+        weights = np.random.normal(0, 0.1, (D, C))
+
+        for it in range(self.max_iters):
+            
+            gradient = self.gradient_logistic_multi(training_data, training_labels, weights)
+            weights = weights - self.lr * gradient
+
+        self.weights = weights
+             
         return self.predict(training_data)
 
     def predict(self, test_data):
@@ -46,9 +117,13 @@ class LogisticRegression(object):
         Returns:
             pred_labels (array): labels of shape (N,)
         """
+    
         ##
         ###
         #### WRITE YOUR CODE HERE! 
         ###
         ##
+        y = self.f_softmax(test_data, self.weights)
+        pred_labels = np.argmax(y, axis = 1)
+
         return pred_labels
